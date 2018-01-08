fc332017c42744468d0c079f5af968d36c6f2bae
RichH <RichH@beacon-optical.co.uk>
2018-01-08 16:31:32 +0000

    Roughed in main parsing logic.  Need separate step to 'reconcile' events into list of clipnames with in and out points.

--- edlParseNew.py
+++ edlParseNew.py
@@ -22,61 +22,193 @@ class EdEvent( object ):
         self.meta = {}
         self.extended = {}
         
+        
 class EdlParse( object ):
     """
     EDL parser
     """
     # Regexs
-    M_EVENT = re.compile(      r"(\d{3})\s+" +                  # Cut id
-                                "(\w+)\s+" +                    # Inner Name
-                                "([av/]+)\s+" +                 # Taking
-                                "(c|d|w\d+)\s+(\w*?)\s" +       # Transition (Optional Duration)
-                                "((?:(?:\d\d)[:;]?){4})\s+" +   # Source In
-                                "((?:(?:\d\d)[:;]?){4})\s+" +   # Source Out
-                                "((?:(?:\d\d)[:;]?){4})\s+" +   # TL In
-                                "((?:(?:\d\d)[:;]?){4})\s*",    # TL Out
-                                re.I )                          # ignore case
+    M_EVENT = re.compile(      r"(\d{3})\s+" +                  # 1 Cut id
+                                "(\w+)\s+" +                    # 2 Inner Name
+                                "([av/]+)\s+" +                 # 3 Taking
+                                "(c|d|w\d+)\s+" +               # 4 Transition
+                                "(\w*?)\s" +                    # 5 Optional Duration
+                                "((?:(?:\d\d)[:;]?){4})\s+" +   # 6 Source In
+                                "((?:(?:\d\d)[:;]?){4})\s+" +   # 7 Source Out
+                                "((?:(?:\d\d)[:;]?){4})\s+" +   # 8 TL In
+                                "((?:(?:\d\d)[:;]?){4})\s*",    # 9 TL Out
+                                re.I )                          #   ignore case
                                 
-    M_CLIPNAMES = re.compile(  r"\*\s" +                        # Start with a *
-                                "(?:(from|to) clip name:)\s+" + # From or to
-                                "([\w\\/\-\.]+)",               # the claip name
-                                re.I )                          # ignore case
+    M_CLIPNAMES = re.compile(  r"\*\s" +                        #   Start with a *
+                                "(?:(from|to) clip name:)\s+" + # 1 From or to
+                                "([\w\\/\-\.]+)",               # 2 the claip name
+                                re.I )                          #   ignore case
                                 
-    M_NAMEDFX = re.compile(    r"^\*?\s?effect.*?(?:is|:)\s?" + # Effect preamble
-                                "([\w \-\\/]+)\s" +             # Effect Name
-                                "(\((?:[\w\d\+\-_ ]*)\))?\s*",  # Optional paramiter
-                                re.I )                          # ignore case
+    M_NAMEDFX = re.compile(    r"^\*?\s?effect.*?(?:is|:)\s?" + #   Effect preamble
+                                "([\w \-\\/]+)\s" +             # 1 Effect Name
+                                "(\((?:[\w\d\+\-_ ]*)\))?\s*",  # 2 Optional paramiter
+                                re.I )                          #   ignore case
                                 
-    M_TIMEWARP = re.compile(   r"m2\s+" +                       # timewarp id
-                                "(\w+)\s+" +                    # inner name
-                                "([-+]?[0-9]*\.?[0-9]+)\s+" +   # new framerate
-                                "((?:(?:\d\d)[:;]?){4})",       # source_in
-                                re.I )                          # ignore case
+    M_TIMEWARP = re.compile(   r"m2\s+" +                       #   timewarp id
+                                "(\w+)\s+" +                    # 1 inner name
+                                "([-+]?[0-9]*\.?[0-9]+)\s+" +   # 2 new framerate
+                                "((?:(?:\d\d)[:;]?){4})",       # 3 source_in
+                                re.I )                          #   ignore case
                                 
-    def __init__( self, file_path=None ):
+    M_DROPPING = re.compile(   r"fcm:\s+" +                     #   Rate preamble
+                                "([\w\-]+)" +                   # 1 DF / NDF
+                                "\s+frame", re.I )              #   Ignore Case
+                                
+    def __init__( self, file_path=None, rate=25 ):
         self.source_file = file_path
         self.sequence_name = None
         self.events = {}
         self.event_order = []
-        self.tc_rate = None
-        
+        self.tc_drop = "NON-DROP"
+        self.tc_rate = rate
+        self.extended = []
         self._interuption = None
         self._fh = None
         
+    def _makeTC( self, tc_string ):
+        tc = TC.Timecode( self.tc_rate, 1 )
+        tc.setFroMStringTC( tc_string )
+        return tc
+        
+    def _makeEvent( self, id, match ):
+        e = EdEvent( id )
+        e.inner_name = match.group(2)
+        e.taking = match.group(3)
+        trans = match.group(4)
+        if( trans != "C" ){
+            # may have transition duration
+            e.extended[ trans ] = match.group(5)
+        e.source_in    = _makeTC( match.group(6) )
+        e.source_out   = _makeTC( match.group(7) )
+        e.timeline_in  = _makeTC( match.group(8) )
+        e.timeline_out = _makeTC( match.group(9) )
+        return e
+        
     def parse( self ):
         # open file
+        fh = open( self.source_file, "rb" )
+        
+        cur_rate = cur_id = None
+        block_hint = False
         
         # read title
-        
-        # test for optional 'format' metadata
-        
-        # test for next event's Dropping standard
-        
-        # test for event fingerprint
-        # test for metadata
-        
-        
+        header = True
+        while( header ):
+            got_match = False
+            line = fh.readline()
+            if( len( line ) < 3 ):
+                # blank line
+                continue
+                
+            match = re.match( r"title:\s+(.*)", line, re.I )
+            if match:
+                self.sequence_name = match.group( 1 ).strip()
+                got_match = True
+                
+            match = self.M_DROPPING.match( line )
+            if match:
+                self.tc_drop = match.group( 1 )
+                cur_rate = self.tc_drop
+                got_match = True
+                
+            match = re.match( r"\*\s+format\s+(\d+x\d+)\s*([ip])\s*(\d+\.?\d*)fps", line, re.I )
+            if match:
+                # extended metadata
+                self.extended.append( match.group(0) )
+                if( self.tc_rate == None ):
+                    self.tc_rate = int( float( match.group(3) ) )
+                got_match = True
+                
+            # take what we can up too the first event.
+            match = self.M_EVENT.match( line ):
+            if match:
+                header = False
+                got_match = True
+            if not got_match:
+                print "Unrecognised Header Line:\n'{}'".format( line )
+                
+        # Now examine list of Edit Events....
+        cur_id = -1
+        fails = 0
+        while( len( line ) > 0 ):
+            match = self.M_DROPPING.match( line )
+            if match:
+                # update dropping
+                new_rate = match.group( 1 )
+                if( new_rate != cur_rate ):
+                    block_hint = True
+                    ` = new_rate
+                line = fh.readline()
+                fails = 0
+            else:
+                fails += 1
+                
+            # test for event
+            match = self.M_EVENT.match( line ):
+            if match:
+                test_id = int( match.group(1) )
+                if( test_id != cur_id ):
+                    # New Event
+                    block_hint = False
+                    cur_id = test_id
+                    e = self._makeEvent( cur_id, match )
+                    e.meta[ "DROPPING" ] = cur_rate
+                    # store
+                    self.events[ cur_id ] = e
+                    self.event_order.append( cur_id )
+                else:
+                    # usually a disolve transition
+                    e = self._makeEvent( cur_id, match )
+                    e.meta[ "DROPPING" ] = cur_rate
+                    self.events[ cur_id ].extended[ "TRANSITION" ] = e
+                line = fh.readline()
+                fails = 0
+            else:
+                fails += 1
+                
+            # test for expected metadata
+            match = self.M_NAMEDFX.match( line )
+            if match:
+                line = fh.readline()
+                fails = 0
+            else:
+                fails += 1
+                
+            # test for ClipName Data
+            match = self.M_CLIPNAMES.match( line )
+            if match:
+                line = fh.readline()
+                fails = 0
+            else:
+                fails += 1
+                
+            # test for a timewarp
+            match = self.M_TIMEWARP.match( line )
+            if match:
+                line = fh.readline
+                fails = 0
+            else:
+                fails += 1
+                
+            # Last resort
+            if( fails > 4 ):
+                # tried all Regexs, this is unknown metadata
+                self.events[ cur_id ].extended[ "UNKNOWN" ] = line
+                line = fh.readline
+                fails = 0
+                
+                
 if __name__ == "__main__":
     # OK GO!
+    import os
     tests = [ "example.edl", "RSG-BLACK-1.edl", "SCENEcombined.edl" ]
-    
+    for test in tests:
+        file_name = os.path.join( "testData", test )
+        edl = EdlParse( file_name )
+        edl.parse()
+        
